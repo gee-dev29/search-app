aae11333e082754217fbaa905e875dd8ca646bcd
gee-dev29 <maduegodwin2605@gmail.com>
2024-09-29 21:34:03 +0100

    refactored

---
+++ interface/bannedIPs.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const bannedIpSchema = new mongoose.Schema({
+    ipAddress: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$|^([0-9a-f]{1,4}:){7}[0-9a-f]{1,4}$/.test(
+                    v
+                );
+            },
+            message: (props) => `${props.value} is not a valid IP address!`,
+        },
+    },
+    reason: {
+        type: String,
+        required: true,
+    },
+    bannedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    offenseCount: {
+        type: Number,
+        default: 0,
+    },
+});
+
+export const BannedIp = mongoose.model("BannedIp", bannedIpSchema);
