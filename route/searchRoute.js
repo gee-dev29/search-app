dc80041fef4bd3274e0b793f1ca2a470ec17cd0b
donemmx <emmanuelidus@gmail.com>
2024-10-02 22:39:27 +0100

    fix

--- route/searchRoute.js
+++ route/searchRoute.js
@@ -1,20 +1,16 @@
 import express from "express";
-import { jwtVerify } from "../middleware/jwtAuthentification.js";
 import { searchData } from "../controller/dataEntryController.js";
 import { searchDataLimiter } from "../middleware/searchDataLimiter.js";
 import { activityLogger } from "../middleware/activityLoggerMiddleware.js";
 const router = express.Router();
 
-router
-    .route("/search-data")
-    .get(
-        jwtVerify,
-        searchDataLimiter,
-        activityLogger(
-            "Search Data Entry",
-            "Search for Church data Entry successful"
-        ),
-        searchData
-    );
+router.route("/").get(
+  // searchDataLimiter,
+  // activityLogger(
+  //     "Search Data Entry",
+  //     "Search for Church data Entry successful"
+  // ),
+  searchData
+);
 
 export default router;
