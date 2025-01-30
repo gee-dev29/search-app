540b44b99b9d777e669a36c9c96a3c773b04bc9e
donemmx <emmanuelidus@gmail.com>
2025-01-30 16:34:00 +0100

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -432,7 +432,7 @@ export const getAllLogs = async (req, res) => {
   try {
 
     
-    const result = await ActivityLog.find()
+    const result = await ActivityLog.find().sort(-1)
     
     return res.status(200).json({
       data: result,
