612a900dc30b004e74c4a65b3ededafaec48ab55
donemmx <emmanuelidus@gmail.com>
2025-02-12 08:29:51 +0100

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -341,6 +341,8 @@ export const verifyOTP = async (req, res) => {
     if (req.body.otp !== user.otp) {
       return res.status(400).json({
         message: "Wrong OTP",
+        otp: req.body.otp,
+        userOtp: user.otp,
       });
     }
 
