bc3f9f3a169efe5167ba4d5b54b4638c935e7f24
donemmx <emmanuelidus@gmail.com>
2025-02-16 09:25:18 +0100

    fix

--- route/dataEntryRoute.js
+++ route/dataEntryRoute.js
@@ -11,25 +11,28 @@ import {
   getDataByStatus,
   getDataEntry,
   getMyAnalytics,
-
 } from "../controller/dataEntryController.js";
 import { jwtVerify } from "../middleware/jwtAuthentification.js";
 import { superAdminRoleCheck } from "../middleware/checkRole.js";
+
 const router = express.Router();
 
+// Routes for Church-related actions
 router.route("/church").post(jwtVerify, checkUser, createChurchEntry);
+router.route("/churches").get(jwtVerify, checkUser, getAllChurches);
+
+// Routes for Branch-related actions
 router.route("/branch").post(jwtVerify, checkUser, createBranchEntry);
+router.route("/branch/:id").get(jwtVerify, checkUser, getBranchById); // Correcting GET for individual branch by ID
+router.route("/all-branches").get(jwtVerify, checkUser, getAllBranches);
+
+// General routes
 router.route("/").get(jwtVerify, checkUser, getAllUserDataEntry);
-router
-  .route("/entry")
-  .get(jwtVerify, checkUser, superAdminRoleCheck, getDataByStatus);
+router.route("/entry").get(jwtVerify, checkUser, superAdminRoleCheck, getDataByStatus);
+router.route("/entry/:id").get(jwtVerify, checkUser, getDataEntry);
 
-  router.route("/my-analytics").get(jwtVerify, checkUser, getMyAnalytics);
-  router.route("/churches").get(jwtVerify, checkUser, getAllChurches);
-  router.route("/entry/:id").get(jwtVerify, checkUser, getDataEntry);
-
-  router.route("/branch").get(jwtVerify, checkUser, getBranchById);
-  router.route("/all-branches").get(jwtVerify, checkUser, getAllBranches);
-  
+// Analytics routes
+router.route("/my-analytics").get(jwtVerify, checkUser, getMyAnalytics);
 router.route("/analytics").get(jwtVerify, checkUser, getAllAnalytics);
+
 export default router;
