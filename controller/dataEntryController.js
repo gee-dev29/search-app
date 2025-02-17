28ff0781e4842dce2989d8edc3a90dec3bb461a5
donemmx <emmanuelidus@gmail.com>
2025-02-17 20:06:24 +0100

    fix

--- controller/dataEntryController.js
+++ controller/dataEntryController.js
@@ -349,8 +349,8 @@ export const getSearchById = async (req, res) => {
     // First, try to find the Church by ID
     let result = await churchModel.findById(id);
     if (result) {
-      const branches = await branchesModel.findById(id);
-      const combinedData = {...branches, result}
+      const branches = await branchesModel.findById(result.id);
+      const combinedData = {...branches, ...result}
       return res.status(200).json({ type: "church", data: combinedData });
     }
 
