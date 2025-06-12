9dccb967891e955eb3c0c2810a393c32c07c0f4a
e-idusuyi <e-idusuyi@leadway.com>
2025-06-12 17:06:12 +0100

    fix

--- middleware/findUserByEmail.js
+++ middleware/findUserByEmail.js
@@ -21,6 +21,9 @@ export const findUserByEmail = async (req, res, next) => {
         if(user.UserStatus == UserStatus.SUSPENDED){
             return res.status(401).json({ message: "Your account has been suspended. contact admin" });
         }
+        if(user.UserStatus == UserStatus.DELETED){
+            return res.status(401).json({ message: "Your account has been deleted. contact admin" });
+        }
         req.user = user._doc;
         next();
     } catch (error) {
